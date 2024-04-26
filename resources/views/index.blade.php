7ec56ccb0bfa37085e5631ce32115d07458714c8
Shinwari <sohailciit38@gmail.com>
2024-04-26 21:47:18 +0500

    welcome message

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,12 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
+        <title>NDA</title>
+            <h1 class="bg-black text-white"> Welcome to the NDS </h1>
     </head>
-   
+
 </html>
