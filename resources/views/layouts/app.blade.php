5e62f490701bba2eaddcb01fd5f719ba5f4a4beb
sohail-khan <sohail8338@gmail.com>
2024-12-05 23:13:00 +0500

    change the js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="{{ asset(''js/app.js) }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 <body>
     <div id="app">
